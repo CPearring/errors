cefe60e5d9103c0e76396dedf9c7ea91fa37bfd0
Casey P <pearring@hawaii.edu>
2015-09-24 17:26:44 -1000

    added errors

--- throw_err.py
+++ throw_err.py
@@ -5,12 +5,14 @@
 
 import sys
 
+
 def print_usage():
     """Print usage and exit"""
-    sys.stderr.write("usage: python raise_err.py <error type>\n")
+    sys.stderr.write("usage: python throw_err.py <error type>\n")
     sys.stderr.write("available errors: \n")
     sys.stderr.write("\tassertion, io, import, index\n")
     sys.stderr.write("\tkey, name, os, type, value,\n")
+    sys.stderr.write("\tattribute, eof, floatingpoint, generatorexit,\n")
     sys.stderr.write("\tzerodivision\n")
     sys.exit()
 
@@ -21,25 +23,61 @@ if len(sys.argv) != 2:
 error_type = sys.argv[1]
 
 if error_type == "assertion":
-    raise AssertionError
+    assert False, 'The assertion failed'
 elif error_type == "io":
-    raise IOError
+    f = open('/does/not/exist', 'r')
 elif error_type == "import":
-    raise ImportError
+    import module_does_not_exist
 elif error_type == "index":
-    raise IndexError
+    my_seq = [ 0, 1, 2 ]
+    print my_seq[3]
 elif error_type == "key":
-    raise KeyError
+    d = { 'a':1, 'b':2 }
+    print d['c']
 elif error_type == "name":
-    raise NameError
+    def func():
+	print unkown_name
+	
+    func()
 elif error_type == "os":
-    raise OSError
+    import os
+    for i in range(10):
+	print i, os.ttyname(i)
 elif error_type == "type":
-    raise TypeError
+    result = ('tuple',) + 'string'
 elif error_type == "value":
-    raise ValueError
+    print chr(1024)
 elif error_type == "zerodivision":
-    raise ZeroDivisionError
+    1 / 0 
+elif error_type == "attribute":
+	class NoAttributes(object):
+		pass
+
+	o = NoAttributes()
+	print o.attribute
+elif error_type == "eof":
+	while True:
+		data = input('prompt:')
+		print 'READ:', data
+elif error_type == "floatingpoint":
+	import math
+	import fpectl
+
+	print 'control off:', math.exp(1000)
+	fpectl.turnon_sigfpe()
+	print 'Control on:', math.exp(1000)
+elif error_type == "generatorexit":
+	def my_generator():
+   		try:
+   	    		for i in range(5):
+  	         		print 'Yielding', i
+				yield i
+  	 	except GeneratorExit:
+  	     		print 'Exiting early'
+
+	g = my_generator()
+	print g.next()
+	g.close()
 else:
     sys.stderr.write("Sorry, not able to throw a(n) ")
     sys.stderr.write(error_type + " error\n")
